45f848edc60a805d910f3a962bfab99fdb89930c
XxJMPPxX <151105147+XxJMPPxX@users.noreply.github.com>
2025-05-03 10:50:43 -0500

    wa
    TERMINE

--- Assets/Menu/Scripts/PlayerManager.cs
+++ Assets/Menu/Scripts/PlayerManager.cs
@@ -1,24 +1,33 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class PlayerManager : MonoBehaviour {
-  PhotonView pv;
-    
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    PhotonView pv;
+    private int selectedAvatarIndex;
 
-  private void Awake() {
-    pv = GetComponent<PhotonView>();
-  }
-
-  private void Start() {
-    if (pv.IsMine) {
-      CreateController();
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
     }
-  }
 
-  private void CreateController() {
-    PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
-  }
+    private void Start()
+    {
+        if (pv.IsMine)
+        {
+            // Obtener el índice del avatar seleccionado desde las propiedades del jugador
+            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("AvatarIndex"))
+            {
+                selectedAvatarIndex = (int)PhotonNetwork.LocalPlayer.CustomProperties["AvatarIndex"];
+            }
+            CreateController();
+        }
+    }
+
+    private void CreateController()
+    {
+        string prefabName = selectedAvatarIndex == 0 ? "PlayerController" : "PlayerController_1";
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabName), Vector3.zero, Quaternion.identity);
+    }
 }
